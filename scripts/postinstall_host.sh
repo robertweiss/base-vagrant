3e2cf9cd55d8f5f9234f745eca1dfc395eeae874
Robert Weiss <post@robertweiss.de>
2018-08-15 22:41:35 +0200

    HTTP as default

--- scripts/postinstall_host.sh
+++ scripts/postinstall_host.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Change Homepage param in package.json and siteTitle in _init.php
-sed -i "s/https:\/\/base.test\//https:\/\/$3\//g" public/site/templates/package.json
+sed -i "s/http:\/\/base.test\//http:\/\/$3\//g" public/site/templates/package.json
 sed -i "s/\$siteTitle = '';/\$siteTitle = '$1';/g" public/site/templates/_init.php
 
 # Open Admin in Chrome
-open https://$3/pw/access/users/edit/?id=41
+open http://$3/pw/access/users/edit/?id=41
